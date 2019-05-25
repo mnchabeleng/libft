526b82e26fae78fe7d487f8a9dc739f5d495bb2c
mnchabeleng <mnchabel@student.wethinkcode.co.za>
2019-05-25 17:51:12 +0200

    modified : ft_strncmp.c

--- src/strncmp/ft_strncmp.c
+++ src/strncmp/ft_strncmp.c
@@ -3,14 +3,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, int n)
 {
+	int i;
+
 	if (s1 && s2)
 	{
-		while (--n && *s1 && *s2 && *s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		return ((unsigned char)*s1 - (unsigned char)*s2);
+		i = 0;
+		while (--n && s1[i] && s2[i] && s1[i] == s2[i])
+			i++;
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	}
 	return (-1);
 }
