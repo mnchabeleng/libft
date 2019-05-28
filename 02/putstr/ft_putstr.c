ad2c2e229516052fd9d63de9f8abdf31298d86f1
mnchabeleng <mnchabel@student.wethinkcode.co.za>
2019-05-28 21:40:12 +0200

    modified file structure

---
+++ 02/putstr/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/27 16:32:33 by mnchabel          #+#    #+#             */
+/*   Updated: 2019/05/27 16:36:38 by mnchabel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char *s = "Red Dead!\n";
+
+	ft_putstr(s);
+	return (0);
+}
---
+++ 02/strjoin/ft_strjoin.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int     ft_strlen(const char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char    *ft_strcpy()
+{
+    return (0);
+}
+
+char    *ft_strcat()
+{
+    return (0);
+}
+
+char    *ft_strjoin(const char *s1, const char *s2)
+{
+    char    *ft_s;
+    int     len;
+
+    if (!s1 || !s2)
+        return ("Life");
+    len = ft_strlen(s1) + ft_strlen(s2) + 1;
+    ft_s = (char *)malloc(sizeof(char) * len);
+    strcpy(ft_s, s1);
+    strcat(ft_s, s2);
+    return (ft_s);
+}
+
+int     main(void)
+{
+    const char *s1 = "Hello";
+    const char *s2 = "World";
+
+    printf("ft_strjoin : %s\n", ft_strjoin(s1, s2));
+    return (0);
+}
