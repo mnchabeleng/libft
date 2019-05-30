a1a814d21b39dc8adbd3a69b2b7df854d7bf0abb
Modibe NCHABELENG <mnchabel@c5r8s8.wethinkcode.co.za>
2019-05-30 11:30:18 +0200

    modified : ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,7 +12,11 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
