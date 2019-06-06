20060e559cae1be545d6260f689e3e4f53bd5908
Modibe NCHABELENG <mnchabel@c5r9s10.wethinkcode.co.za>
2019-06-06 10:49:16 +0200

    modified : ft_strequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 14:56:49 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/04 14:59:50 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/06/06 10:47:46 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	if (ft_memcmp(s1, s2, ft_strlen(s1)))
+	if (ft_strcmp(s1, s2) == ft_strcmp(s2, s1))
 		return (1);
 	return (0);
 }
