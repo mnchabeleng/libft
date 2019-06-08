6f8fc8ca0c363839df7e0413db84b23a22135380
Modibe NCHABELENG <mnchabel@c5r9s10.wethinkcode.co.za>
2019-06-08 15:32:40 +0200

    modified : ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/06 14:50:47 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/08 15:31:06 by mnchabel         ###   ########.fr       */
+/*   Created: 2019/06/08 13:46:17 by mnchabel          #+#    #+#             */
+/*   Updated: 2019/06/08 13:47:45 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char	**ft_strsplit(char const *s, char c)
 {
-	char	*ft_s;
-	size_t	i;
-	size_t	j;
-
-	if (s)
-	{
-		i = 0;
-		j = ft_strlen(s);
-		while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-			i++;
-		while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
-			j--;
-		ft_s = ft_strsub(s, i, j);
-		return (ft_s);
-	}
-	return (0);
 }
