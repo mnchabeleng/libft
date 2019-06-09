a10848406bfb3248a1cc5e670bdfe42d44068935
Modibe NCHABELENG <mnchabel@c5r9s10.wethinkcode.co.za>
2019-06-09 15:38:55 +0200

    modified : ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 14:50:47 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/08 16:59:34 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/06/09 15:36:29 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_strtrim(char const *s)
 {
-	char	*ft_s;
 	size_t	i;
 	size_t	j;
 
@@ -22,10 +21,11 @@ char	*ft_strtrim(char const *s)
 		return (0);
 	i = 0;
 	j = ft_strlen(s);
-	while ((s[i] >= '\t' && s[i] <= '\r') || s[i] == 32)
+	while (s[i] && (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'))
 		i++;
-	while ((s[j - 1] >= '\t' && s[j - 1] <= '\r') || s[j - 1] == 32)
+	while (i < j && (s[j - 1] == ' ' || s[j - 1] == '\n' || s[j - 1] == '\t'))
 		j--;
-	ft_s = ft_strsub(s, i, j);
-	return (ft_s);
+	if (i == j)
+		return (ft_strnew(1));
+	return (ft_strsub(s, i, j - i));
 }
