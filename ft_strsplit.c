76fa466562a50669bab3ec74f4ac2c65782accbb
Modibe NCHABELENG <mnchabel@c5r9s10.wethinkcode.co.za>
2019-06-10 16:58:07 +0200

    modified : ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,14 +6,19 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 13:46:17 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/10 15:11:51 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:57:21 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_word_count()
+int		ft_word_count(char *s, char c)
 {
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	j = 0;
 	return (0);
 }
 
