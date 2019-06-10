4d6acdb55f596f907348df3d1599b58c10894a12
mnchabeleng <mnchabel@student.wethinkcode.co.za>
2019-06-11 00:17:54 +0200

    modified : ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,17 +12,52 @@
 
 #include "libft.h"
 
-int		ft_word_count(char *s, char c)
+int	ft_word_count(const char *s, char c)
 {
-	unsigned int i;
-	unsigned int j;
+	int i;
+	int temp;
 
 	i = 0;
-	j = 0;
-	return (0);
+	temp = 0;
+	while (*s)
+	{
+		if (temp == 1 && *s == c)
+			temp = 0;
+		if (temp == 0 && *s != c)
+		{
+			temp = 1;
+			i++;
+		}
+		s++;
+	}
+	return (i);
 }
 
 char	**ft_strsplit(char const *s, char c)
 {
-	return (0);
+	char		**ft_s;
+	unsigned int 	i;
+	unsigned int 	j;
+	unsigned int 	len;
+	unsigned int	start;
+
+
+	if (!s || !c)
+		return (0);
+	len = ft_word_count(s, c) + 1;
+	ft_s = (char)malloc(sizeof(**ft_s) * len);
+	i = 0;
+	j = -1;
+	while (++j < len)
+	{
+		while (is[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		ft_s[i] = ft_strsub(s, start, i - start);
+		i++;
+	}
+	ft_s[j] = 0;
+	return (ft_s);
 }
