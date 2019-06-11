44942dc8fbf93a7fe9517e58bfb3c985cbf6641b
mnchabeleng <mnchabel@student.wethinkcode.co.za>
2019-06-12 00:04:26 +0200

    modified : ft_itoa.c, ft_strsplit.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,4 +12,48 @@
 
 #include "libft.h"
 
-char	*ft_itoa(int n);
+char	*ft_strrev(char *s)
+{
+	char	temp;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (len > 0)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+	return (s);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int	sign;
+	int	i;
+
+	if (n == 0)
+		return (0);
+	if (n == -2147483648)
+		return ((char *)"-2147483648");
+	if (n < 0)
+		sign = -1;
+	res = ft_strnew(12);
+	if (!res)
+		return (0);	
+	i = 0;
+	while (n > 0)
+	{
+		res[i] = n % 10 + '0';
+		n /= 10;
+		i++;
+	}
+	if (sign)
+		res[i++] = '-';
+	return (ft_strrev(res));
+}
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,4 +12,38 @@
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c);
+unsigned int	ft_word_count(char *s, char c)
+{
+	unsigned int	count;
+	size_t		i;
+
+	count = 1;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			while (s[i] == c)
+				i++;
+			count++;
+		}
+		if (s[i])
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char		*ft_s;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+
+	ft_s = (char **)malloc(sizeof(*ft_s) * ft_word_count(s, c));
+	if (!s || !ft_s)
+		return (0);
+	i = 0;
+	j = 0;
+	k = 0;
+}
