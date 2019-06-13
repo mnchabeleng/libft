cad75f73fe3fab7f1a409da65bee1b67f246488a
Modibe NCHABELENG <mnchabel@c5r9s10.wethinkcode.co.za>
2019-06-13 15:30:17 +0200

    modified : ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,46 +6,37 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 13:46:17 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/12 12:01:11 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/06/13 14:28:11 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-unsigned int	ft_word_count(char *s, char c)
+char	**ft_strsplit(const char *s, char c)
 {
-	unsigned int	count;
-	size_t		i;
+	char	**ft_s;
+	size_t	i;
+	size_t	j;
+	size_t	len;
 
-	count = 1;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-		{
-			while (s[i] == c)
-				i++;
-			count++;
-		}
-		if (s[i])
-			i++;
-	}
-	return (count);
-}
-
-char	**ft_strsplit(char const *s, char c)
-{
-	char		*ft_s;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	k;
-
-	ft_s = (char **)malloc(sizeof(*ft_s) * ft_word_count(s, c));
-	if (!s || !ft_s)
+	ft_s = ft_memalloc(ft_strlen(s) + 1);
+	if (!ft_s)
 		return (0);
 	i = 0;
 	j = 0;
-	k = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			len = 0;
+			while (s[len + i] && (s[len + i] != c))
+				len++;
+			ft_s[j++] = ft_strsub(s, i, len);
+			i += len;
+		}
+	}
+	ft_s[j] = 0;
+	return (ft_s);
 }
-*/
